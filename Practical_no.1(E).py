fb9874575d83e12551252e0eea7dc30f3ce08978
Dev <codekidinprogress@gmail.com>
2026-01-12 23:53:04 +0530

    Add Practical No. 1(E) Python program
    Aim is to write a function to check the input value is Armstrong and also write the function for
    Palindrome.

---
+++ Practical_no.1(E).py
@@ -0,0 +1,36 @@
+def is_armstrong(num):
+    total = 0
+    n = num
+    power = len(str(num))
+
+    while n > 0:
+        digit = n % 10
+        total += digit ** power
+        n //= 10
+
+    return total == num
+
+
+def is_palindrome(num):
+    original = num
+    rev = 0
+
+    while num > 0:
+        digit = num % 10
+        rev = rev * 10 + digit
+        num //= 10
+
+    return rev == original
+
+
+number = int(input("Enter a number: "))
+
+if is_armstrong(number):
+    print(number, "is an Armstrong number")
+else:
+    print(number, "is not an Armstrong number")
+
+if is_palindrome(number):
+    print(number, "is a palindrome number")
+else:
+    print(number, "is not a palindrome number")
